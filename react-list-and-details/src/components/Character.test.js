7c2d24a7a00ad3f30e30b899ccde4ed045cc1a1b
Tis Lais <tistististis@gmail.com>
2021-07-27 17:31:22 -0700

    Add snapshot test for Character

--- react-list-and-details/src/components/Character.test.js
+++ react-list-and-details/src/components/Character.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import Character from './Character';
 
-test('renders learn react link', () => {
-  render(<Character />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Character component', () => {
+  it('renders Character', () => {
+    const { asFragment } = render(<Character
+      name="46th Earth King"
+      affilition=" Earth Kingdom Earth Kingdom Royal Family"
+      allies={["Royal Earthbender Guards"]}
+      enemies={["Chin"]}
+      photoUrl="https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441" />);
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
